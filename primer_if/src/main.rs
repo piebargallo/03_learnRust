40d3e14cb8d47a35b3ea0ff7e17b379a2effc9c9
MIGUEL PIE BARGALLO <90567646+piebargallo@users.noreply.github.com>
2025-01-29 11:37:31 +0100

    Add files via upload

---
+++ primer_if/src/main.rs
@@ -0,0 +1,15 @@
+fn main() {
+    let numero = 8;
+    let number = 4;
+
+    // Condición booleana
+    if numero < 5 {
+        println!("Condición verdareda");
+    } else {
+        println!("Condición falsa");
+    }
+
+    if number != 0 {
+        println!("El número pasado no es cero");
+    }
+}
